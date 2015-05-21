27551a8f5866ab5652e3930393d27f6ca25f7b79
bhennes2 <bhennes2@gmail.com>
2015-05-21 11:50:38 -0500

    Got basic front-end working

--- app.js
+++ app.js
@@ -6,29 +6,55 @@ if (Meteor.isServer) {
 
 if (Meteor.isClient) {
 
-  var score = [0, 0];
+  var score = [0, 0],
+      newGame = true,
+      gameOver = false;
 
   $(document).on('keyup', function (e) {
-    var path = window.location.pathname;
+    var path = window.location.pathname,
+        $gameMessage = $(".game-message"),
+        $servingMessages = $('.serving-message');
     if (path === "/"){
       window.location.pathname = "/play_game";
       return
     }
-    if (e.keyCode == 49){
-      score[0] += 1;
-      $('.score:first').text(score[0]);
-    } else if (e.keyCode == 50){
-      score[1] += 1;
-      $('.score:last').text(score[1]);
+
+    if (newGame){
+      if (e.keyCode == 65){
+        $servingMessages.filter(':first').removeClass('hide');
+      } else if (e.keyCode == 66){
+        $servingMessages.filter(':last').removeClass('hide');
+      }
+      newGame = false;
+      $gameMessage.addClass('hide');
+      return;
     }
-    var totalScore = score[0] + score[1],
-        message = $('.game-message p');
-    if (score[0] >= 21 && (score[0] - score[1]) > 1){
-      message.text("Player 1 wins!");
-    } else if (score[1] >= 21 && (score[1] - score[0]) > 1) {
-      message.text("Player 2 wins!");
-    } else if (totalScore % 5 === 0){
-      message.text("Change server!");
+
+    if (!gameOver){
+      if (e.keyCode == 65){
+        score[0] += 1;
+        $('.score:first').text(score[0]);
+      } else if (e.keyCode == 66){
+        score[1] += 1;
+        $('.score:last').text(score[1]);
+      }
+      var totalScore = score[0] + score[1],
+          message = $('.game-message p');
+      if (score[0] >= 21 && (score[0] - score[1]) > 1){
+        $gameMessage.removeClass('hide');
+        message.text("Player 1 wins!");
+        gameOver = true;
+      } else if (score[1] >= 21 && (score[1] - score[0]) > 1) {
+        $gameMessage.removeClass('hide');
+        message.text("Player 2 wins!");
+        gameOver = true;
+      } else if (totalScore % 5 === 0 && totalScore > 0){
+        $gameMessage.removeClass('hide');
+        message.text("Change server!");
+        $servingMessages.toggleClass('hide');
+      } else {
+        $gameMessage.addClass('hide');
+      }
     }
   });
 
--- components.jsx
+++ components.jsx
@@ -4,3 +4,39 @@ var PlayGameLink = ReactMeteor.createClass({
     return <p className="menu-item game-start"><a href="/play_game">Play</a></p>;
   }
 });
+
+var ServingMessage = ReactMeteor.createClass({
+  templateName: "ServingMessage",
+  render: function() {
+    var classes = 'serving-message';
+    if (this.props.show === "false"){
+      classes += ' hide';
+    }
+    return <span className={classes}>Serving</span>;
+  }
+});
+
+var Game = ReactMeteor.createClass({
+  templateName: "Game",
+  render: function(){
+    return (
+      <div className="game-container">
+        <div className="player-container left">
+          <p className="player-name">Player 1</p>
+          <h1 className="score">0</h1>
+          <ServingMessage show="false" />
+        </div>
+        <div className="player-container right">
+          <p className="player-name">Player 2</p>
+          <h1 className="score">0</h1>
+          <ServingMessage show="false" />
+        </div>
+        <div className="game-message">
+          <p>Who serves first?</p>
+        </div>
+      </div>
+    );
+  }
+});
+
+
