cb214bda5cb2db4875d1a8fd326ab9f1c3770527
bhennes2 <bhennes2@gmail.com>
2015-10-06 08:47:22 -0500

    Added skeleton CSS framework meteor package

---
+++ client/components/admin.jsx
@@ -0,0 +1,11 @@
+Admin = React.createClass({
+
+  render() {
+
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+});
---
+++ client/components/dashboard.jsx
@@ -0,0 +1,11 @@
+Dashboard = React.createClass({
+
+  render() {
+
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+});
