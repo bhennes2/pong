8d6c539fb54806e4118c3a09e2cfcc2157272821
David Corwin <dave@launchpadlab.com>
2015-10-05 21:42:26 -0500

    leeeaderboard

--- server/startup.js
+++ server/startup.js
@@ -1,9 +1,19 @@
 if (Meteor.isServer) {
   Meteor.startup(function () {
     if (Players.find().count() === 0) {
-      var names = ['Dave', 'Brendan', 'Tom', 'Scott', 'Ryan', 'Monique', 'Paul', 'Wade'];
-      names.forEach(function (name) {
-        Players.insert({ name: name });
+      var players = [
+        { name: 'Dave',    wins: 5, losses: 5, taunt: 'Muahahaha'},
+        { name: 'Brendan', wins: 5, losses: 1, taunt: 'Muahahaha'},
+        { name: 'Tom',     wins: 5, losses: 2, taunt: 'Muahahaha'},
+        { name: 'Scott',   wins: 2, losses: 4, taunt: 'Muahahaha'},
+        { name: 'Ryan',    wins: 4, losses: 3, taunt: 'Muahahaha'},
+        { name: 'Monique', wins: 4, losses: 4, taunt: 'Muahahaha'},
+        { name: 'Paul',    wins: 2, losses: 2, taunt: 'Muahahaha'},
+        { name: 'Wade',    wins: 1, losses: 1, taunt: 'Muahahaha'}
+      ];
+
+      players.forEach(function (player) {
+        Players.insert(player);
       });
     }
   });
