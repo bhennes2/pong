e1c61f056060570118b68537171ab44439b349f9
David Corwin <dave@launchpadlab.com>
2015-10-05 21:47:20 -0500

    updated player collection to include email

--- server/startup.js
+++ server/startup.js
@@ -2,14 +2,14 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     if (Players.find().count() === 0) {
       var players = [
-        { name: 'Dave',    wins: 5, losses: 5, taunt: 'Muahahaha'},
-        { name: 'Brendan', wins: 5, losses: 1, taunt: 'Muahahaha'},
-        { name: 'Tom',     wins: 5, losses: 2, taunt: 'Muahahaha'},
-        { name: 'Scott',   wins: 2, losses: 4, taunt: 'Muahahaha'},
-        { name: 'Ryan',    wins: 4, losses: 3, taunt: 'Muahahaha'},
-        { name: 'Monique', wins: 4, losses: 4, taunt: 'Muahahaha'},
-        { name: 'Paul',    wins: 2, losses: 2, taunt: 'Muahahaha'},
-        { name: 'Wade',    wins: 1, losses: 1, taunt: 'Muahahaha'}
+        { name: 'Dave',    wins: 5, losses: 5, taunt: 'Muahahaha', email: 'dave@launchpadlab.com'},
+        { name: 'Brendan', wins: 5, losses: 1, taunt: 'Muahahaha', email: 'brendan@launchpadlab.com'},
+        { name: 'Tom',     wins: 5, losses: 2, taunt: 'Muahahaha', email: 'tom@launchpadlab.com'},
+        { name: 'Scott',   wins: 2, losses: 4, taunt: 'Muahahaha', email: 'scott@launchpadlab.com'},
+        { name: 'Ryan',    wins: 4, losses: 3, taunt: 'Muahahaha', email: 'ryan@launchpadlab.com'},
+        { name: 'Monique', wins: 4, losses: 4, taunt: 'Muahahaha', email: 'monique@launchpadlab.com'},
+        { name: 'Paul',    wins: 2, losses: 2, taunt: 'Muahahaha', email: 'paul@launchpadlab.com'},
+        { name: 'Wade',    wins: 1, losses: 1, taunt: 'Muahahaha', email: 'wade@launchpadlab.com'}
       ];
 
       players.forEach(function (player) {
