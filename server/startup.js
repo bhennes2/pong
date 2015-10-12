43e519f343ec5af13059c6eb28a8637857aaa720
bhennes2 <bhennes2@gmail.com>
2015-10-12 15:41:31 -0500

    Updated seeds file

--- server/startup.js
+++ server/startup.js
@@ -2,14 +2,14 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     if (Players.find().count() === 0) {
       var players = [
-        { name: 'Dave',    wins: 5, losses: 5, taunt: 'Muahahaha', email: 'dave@launchpadlab.com'},
-        { name: 'Brendan', wins: 5, losses: 1, taunt: 'Muahahaha', email: 'brendan@launchpadlab.com'},
-        { name: 'Tom',     wins: 5, losses: 2, taunt: 'Muahahaha', email: 'tom@launchpadlab.com'},
-        { name: 'Scott',   wins: 2, losses: 4, taunt: 'Muahahaha', email: 'scott@launchpadlab.com'},
-        { name: 'Ryan',    wins: 4, losses: 3, taunt: 'Muahahaha', email: 'ryan@launchpadlab.com'},
-        { name: 'Monique', wins: 4, losses: 4, taunt: 'Muahahaha', email: 'monique@launchpadlab.com'},
-        { name: 'Paul',    wins: 2, losses: 2, taunt: 'Muahahaha', email: 'paul@launchpadlab.com'},
-        { name: 'Wade',    wins: 1, losses: 1, taunt: 'Muahahaha', email: 'wade@launchpadlab.com'}
+        { name: 'Dave',    wins: 2, losses: 0, taunt: 'Muahahaha', email: 'dave@launchpadlab.com'},
+        { name: 'Ryan',    wins: 0, losses: 2, taunt: 'Muahahaha', email: 'ryan@launchpadlab.com'},
+        { name: 'Brendan', wins: 0, losses: 0, taunt: 'Muahahaha', email: 'brendan@launchpadlab.com'},
+        { name: 'Tom',     wins: 0, losses: 0, taunt: 'Muahahaha', email: 'tom@launchpadlab.com'},
+        { name: 'Scott',   wins: 0, losses: 0, taunt: 'Muahahaha', email: 'scott@launchpadlab.com'},
+        { name: 'Monique', wins: 0, losses: 0, taunt: 'Muahahaha', email: 'monique@launchpadlab.com'},
+        { name: 'Paul',    wins: 0, losses: 0, taunt: 'Muahahaha', email: 'paul@launchpadlab.com'},
+        { name: 'Wade',    wins: 0, losses: 0, taunt: 'Muahahaha', email: 'wade@launchpadlab.com'}
       ];
 
       var dbPlayers = [];
@@ -21,11 +21,21 @@ if (Meteor.isServer) {
       Games.insert({
         player1: dbPlayers[0],
         player2: dbPlayers[1],
-        player1Score: 15,
-        player2Score: 21,
+        player1Score: 21,
+        player2Score: 18,
         firstServer: dbPlayers[1],
         inProgress: false,
-        createdAt: new Date()
+        createdAt: new Date(2015, 9, 12, 11)
+      });
+
+      Games.insert({
+        player1: dbPlayers[0],
+        player2: dbPlayers[1],
+        player1Score: 21,
+        player2Score: 15,
+        firstServer: dbPlayers[1],
+        inProgress: false,
+        createdAt: new Date(2015, 9, 11, 30)
       });
     }
   });
