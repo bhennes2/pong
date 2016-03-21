e16b98b0c099514e02135f9065daca66131cc2aa
bhennes2 <bhennes2@gmail.com>
2016-03-21 13:16:46 -0500

    Started work on guest game

--- client/components/routes.jsx
+++ client/components/routes.jsx
@@ -16,6 +16,7 @@ Routes = React.createClass({
           <Route path="main"          component={MainMenu} />
           <Route path="leaders"       component={Leaders} />
           <Route path="game/new"      component={ChoosePlayerMenu} />
+          <Route path="game/guest"    component={GuestPlay} />
           <Route path="game/:id"      component={ServingMenu} />
           <Route path="game/:id/play" component={Play} />
         </Route>
